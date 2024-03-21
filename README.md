51d9c42d34e66e7fe58ef6c280a19c3be653c8e4
Jiwon Kwon <kjiwon411@gmail.com>
2024-03-21 18:48:03 +0900

    wizards-and-warriors #1 complete
    Add a method

--- README.md
+++ README.md
@@ -1,2 +1,9 @@
 # Exercism_solutions
-#exercism_solutions
+
+This is the repo that contains my Exercism solutions.
+You can solve your own problems in [https://exercism.org/dashboard]
+
+p.s.
+
+You can add your solutions to git. 
+Check this tutorial : [https://www.linkedin.com/pulse/how-setup-exercism-cli-use-your-own-github-repository-jim-lynch/]
---
+++ java/wizards-and-warriors-2/src/main/java/GameMaster.java
@@ -0,0 +1,15 @@
+public class GameMaster {
+
+    // TODO: define a 'describe' method that returns a description of a Character
+    public void describe(Character character) {
+        System.out.print("You're a level " + character.getLevel() + " " + character.getCharacterClass() + " " + "with " + character.getHitPoints() + " hit points.");
+    }
+
+    // TODO: define a 'describe' method that returns a description of a Destination
+
+    // TODO: define a 'describe' method that returns a description of a TravelMethod
+
+    // TODO: define a 'describe' method that returns a description of a Character, Destination and TravelMethod
+
+    // TODO: define a 'describe' method that returns a description of a Character and Destination
+}
