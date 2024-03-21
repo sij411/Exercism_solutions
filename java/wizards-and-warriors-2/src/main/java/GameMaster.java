c7edaa16885752fa223299a413902a2126d9e57e
Jiwon Kwon <kjiwon411@gmail.com>
2024-03-21 18:50:02 +0900

    #2 complete

--- java/wizards-and-warriors-2/src/main/java/GameMaster.java
+++ java/wizards-and-warriors-2/src/main/java/GameMaster.java
@@ -6,6 +6,9 @@ public class GameMaster {
     }
 
     // TODO: define a 'describe' method that returns a description of a Destination
+    public void describe(Destination destination) {
+        System.out.println("You've arrived at " + destination.getName() +", which has " + destination.getInhabitants() + " inhabitants.");
+    }
 
     // TODO: define a 'describe' method that returns a description of a TravelMethod
 
