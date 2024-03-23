1a55640a3282f7b1754e7ae986e1024e67d869fe
Jiwon Kwon <kjiwon411@gmail.com>
2024-03-23 17:24:33 +0900

    Leap 구현 & 테스트 완료

--- java/leap/src/main/java/Leap.java
+++ java/leap/src/main/java/Leap.java
@@ -1,7 +1,13 @@
 class Leap {
 
     boolean isLeapYear(int year) {
-        // 4로 나누어 떨
+        // 4로 나누어 떨어져야 함
+        // 100으로 나누어 떨어진다면, 400으로도 나누어 떨어져야 함
+        if (year % 4 != 0)
+            return false;
+        if (year % 100 == 0 && year % 400 != 0)
+            return false;
+        return true;
     }
 
 }
