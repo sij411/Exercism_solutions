48d054666c9fd542faf5a9fcc157a1746d00a728
Jiwon Kwon <kjiwon411@gmail.com>
2024-03-23 17:00:18 +0900

    Secrets 구현 & 테스트 완료

---
+++ java/leap/src/main/java/Leap.java
@@ -0,0 +1,7 @@
+class Leap {
+
+    boolean isLeapYear(int year) {
+        // 4로 나누어 떨
+    }
+
+}
---
+++ java/perfect-numbers/src/main/java/NaturalNumber.java
@@ -0,0 +1,9 @@
+class NaturalNumber {
+    NaturalNumber(int number) {
+        throw new UnsupportedOperationException("Delete this statement and write your own implementation");
+    }
+
+    Classification getClassification() {
+        throw new UnsupportedOperationException("Delete this statement and write your own implementation.");
+    }
+}
