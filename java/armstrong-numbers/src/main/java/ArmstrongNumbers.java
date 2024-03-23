a1656cbb5b0b12356a0373b36d12bbb96be356e1
Jiwon Kwon <kjiwon411@gmail.com>
2024-03-24 03:40:29 +0900

    Armstrong Numbers 구현 & 테스트 완료

--- java/armstrong-numbers/src/main/java/ArmstrongNumbers.java
+++ java/armstrong-numbers/src/main/java/ArmstrongNumbers.java
@@ -1,9 +1,27 @@
 class ArmstrongNumbers {
 
-    boolean isArmstrongNumber(int numberToCheck) {
+    static boolean isArmstrongNumber(int numberToCheck) {
+        int number = numberToCheck;
 
-        throw new UnsupportedOperationException("Delete this statement and write your own implementation.");
+       // n번째 자릿수 ^ n
+        int sum = 0;
+        int i = 1;
+        while (number >= 10) {
+            number = number / 10;
+            i++;
+        }
 
+        int n = numberToCheck;
+        for (int j = 1; j <= i; j++) {
+            int r = n % 10;
+            sum += (int) Math.pow(r, i);
+            n = n / 10;
+
+        }
+
+        return numberToCheck == sum;
     }
+
+
 
 }
