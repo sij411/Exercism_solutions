2ffd9b0a1d85e5d62ff219d0a86f29f4297f8423
Jiwon Kwon <kjiwon411@gmail.com>
2024-03-23 18:02:04 +0900

    Reverse String 구현 & 테스트 완료

--- java/reverse-string/src/main/java/ReverseString.java
+++ java/reverse-string/src/main/java/ReverseString.java
@@ -1,7 +1,11 @@
 class ReverseString {
 
     String reverse(String inputString) {
-        throw new UnsupportedOperationException("Delete this statement and write your own implementation.");
+        StringBuilder output = new StringBuilder();
+        for (int i = inputString.length() - 1; i >= 0; i--) {
+            output.append(inputString.charAt(i));
+        }
+        return output.toString();
     }
   
 }
