e427f46b675b0c3323a51985f30b553c382aa237
Jiwon Kwon <kjiwon411@gmail.com>
2024-03-29 22:16:43 +0900

    resistor-color 기능 구현 & 테스트 통과 완료

--- java/resistor-color/src/main/java/ResistorColor.java
+++ java/resistor-color/src/main/java/ResistorColor.java
@@ -1,9 +1,28 @@
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+import java.util.Map;
+import java.util.Set;
+
 class ResistorColor {
+
+    private Map<String, Integer> mnemonics = Map.of("black", 0, "brown", 1, "red", 2, "orange", 3,
+            "yellow", 4, "green", 5, "blue", 6, "violet", 7, "grey", 8, "white", 9
+    );
+
     int colorCode(String color) {
-        throw new UnsupportedOperationException("Delete this statement and write your own implementation.");
+        return mnemonics.get(color);
+
     }
 
     String[] colors() {
-        throw new UnsupportedOperationException("Delete this statement and write your own implementation.");
+        String[] colors = new String[mnemonics.size()];
+       for (String color : mnemonics.keySet()){
+           int idx = mnemonics.get(color);
+           colors[idx] = color;
+
+       }
+
+return colors;
     }
 }
