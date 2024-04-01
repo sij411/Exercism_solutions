81f19c593e98d46e2d041d6e0beb717576f394d1
Jiwon Kwon <kjiwon411@gmail.com>
2024-04-01 23:52:32 +0900

    ResistorColorTrio 수정 & 테스트 통과

--- java/resistor-color-trio/src/main/java/ResistorColorTrio.java
+++ java/resistor-color-trio/src/main/java/ResistorColorTrio.java
@@ -6,26 +6,19 @@ class ResistorColorTrio {
             "yellow", 4, "green", 5, "blue", 6, "violet", 7, "grey", 8, "white", 9
     );
 
-    int value(String[] colors) {
-        String ans = "";
-        for (int i = 0; i < 2; i++){
-            String color = colors[i];
-            ans += Integer.toString(mnemonics.get(color));
-        }
-        return Integer.parseInt(ans);
-    }
     String label(String[] colors) {
         // two digits using method : value
         int first = mnemonics.get(colors[0]);
         int second = mnemonics.get(colors[1]);
         int exp = mnemonics.get(colors[2]);
         // calculate exponents with 10
-        int exponents = (int) Math.pow(10, exp);
+        double exponents =  Math.pow(10, exp);
         // calculate total ohm
-        int ohm = first * exponents + second * exponents;
+        // BigInteger ohm = new BigInteger(String.valueOf(first * 10 * exponents + second * exponents));
 
-        // labeling follows metric prefix
+        Long ohm = (long) (first * 10 * exponents + second * exponents);
         String label = "ohms";
+
         if (ohm < Math.pow(10, 3)) {
             return ohm + " " + label;
         }
