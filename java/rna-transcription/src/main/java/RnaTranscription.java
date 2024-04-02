8c6ab9be837731e44d483471bf70d8ac40e42446
Jiwon Kwon <kjiwon411@gmail.com>
2024-04-02 23:49:45 +0900

    rna transcription 구현 완료

--- java/rna-transcription/src/main/java/RnaTranscription.java
+++ java/rna-transcription/src/main/java/RnaTranscription.java
@@ -1,7 +1,21 @@
+import java.util.Map;
+
 class RnaTranscription {
+    private Map<String, String> nucleotides = Map.of(
+            "G", "C", "C", "G", "T", "A", "A", "U"
+    );
 
     String transcribe(String dnaStrand) {
-        throw new UnsupportedOperationException("Delete this statement and write your own implementation.");
+        String rnaStrand = "";
+        if (dnaStrand.isEmpty())
+            return rnaStrand;
+        else {
+            for (int i = 0; i < dnaStrand.length(); i++) {
+                String dna = String.valueOf(dnaStrand.charAt(i));
+                rnaStrand += nucleotides.get(dna);
+            }
+            return rnaStrand;
+        }
     }
 
 }
